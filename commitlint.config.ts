529c15473be982fa2597612c8e3c5761bcbb7d20
Sapna Sharma <sapna.sharma220516@gmail.com>
2025-01-29 23:36:53 +0530

    fix: testing

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,13 +1,17 @@
-module.exports = { extends: ['@commitlint/config-conventional'],
+import { UserConfig } from '@commitlint/types';
+
+const config: UserConfig = {
+    extends: ['@commitlint/config-conventional'],
     rules: {
-        // Define custom rules for commit message format
-        'header-max-length': [2, 'always', 72],  // Maximum length of the header (first line)
+        'header-max-length': [2, 'always', 72],
         'type-enum': [
             2,
             'always',
-            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],  // Custom types
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
         ],
-        'subject-case': [2, 'never', ['start-case', 'pascal-case']],  // Custom subject format
-        'type-case': [2, 'always', 'lower-case'], // Ensure commit type is lowercase
+        'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+        'type-case': [2, 'always', 'lower-case'],
     },
- };
+};
+
+export default config;
